cb6b8450f4e147fca615c9544d1063690abcdf03
SAQLAIN NAWSHAD <saqlainnawshad004@gmail.com>
2025-11-28 03:16:44 +0530

    added Player Moves History Update Feature

--- main.js
+++ main.js
@@ -1,25 +1,60 @@
+const moves = [
+    {
+        icon: document.querySelector(".moveOne"),
+        value: 1,
+        img: "./images/one.png"
+    },
+    {
+        icon: document.querySelector(".moveTwo"),
+        value: 2,
+        img: "./images/two.png"
+    },
+    {
+        icon: document.querySelector(".moveThree"),
+        value: 3,
+        img: "./images/three.png"
+    },
+    {
+        icon: document.querySelector(".moveFour"),
+        value: 4,
+        img: "./images/four.png"
+    },
+    {
+        icon: document.querySelector(".moveFive"),
+        value: 5,
+        img: "./images/five.png"
 
+    },
+    {
+        icon: document.querySelector(".moveSix"),
+        value: 6,
+        img: "./images/six.png"
 
-const one = document.querySelector(".moveOne")
-const two = document.querySelector(".moveTwo")
-const three = document.querySelector(".moveThree")
-const four = document.querySelector(".moveFour")
-const five = document.querySelector(".moveFive")
-const six = document.querySelector(".moveSix")
+    }
+]
 
-const throwButton = document.querySelector(".moveButton")
+for (let i = 0; i < moves.length; i++) {
+    moves[i].icon.onclick = () => {
+        mtile.remove()
+        const mBox = document.createElement('div');
+        mBox.className = "moveHist-Tile";
+        mBox.innerHTML = `
+            <div class="botMove">
+                <div class="bot-icon"></div>
+                <p class="bot-moveNumber">0</p>
+            </div>
+            <div class="playerMove">
+                <div class="player-icon"></div>
+                <p class="player-moveNumber">${moves[i].value}</p>
+            </div>
+        `;
+        mHistory.appendChild(mBox);
 
-const tossDiv = document.querySelector(".tossDiv")
-
-
-const start = document.querySelector(".Start")
-start.onclick = () => {
-    start.style.color = "white";
-    start.style.backgroundColor ="grey";
-    tossDiv.classList.remove("hidden");
-    alert("GAME STARTS! ^_^");
+        mHistory.scrollTop = mHistory.scrollHeight;
+    }
 }
 
 
 
-
+const mtile = document.querySelector(".goneJS")
+const mHistory = document.querySelector(".moveHistory")
